83dcb62eaef35971fe09c206ae0756d01d37fcf0
Gustavo Miranda <gustavocataldo@id.uff.br>
2022-09-22 20:17:56 -0300

    mudanças na classe Piece
    - atributo id agora é territoryId
    - o tamanho do frame agora acompanha o raio da bolinha

--- classes/Piece.py
+++ classes/Piece.py
@@ -5,14 +5,14 @@ PIECE_IMG = pygame.Surface((30,30), pygame.SRCALPHA)
 
 
 class Piece(pygame.sprite.Sprite):
-    def __init__(self, color, pos_x, pos_y, troops, id):
+    def __init__(self, color, pos_x, pos_y, troops, territoryId):
         super().__init__()
-        self.id = id
+        self.territoryId = territoryId
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.troops = troops
         self.radius = 15
-        self.frame = pygame.Surface((30,30), pygame.SRCALPHA)
+        self.frame = pygame.Surface((self.radius*2, self.radius*2), pygame.SRCALPHA)
         pygame.draw.circle(self.frame, color, (self.radius, self.radius), self.radius, 0)
         self.rect = self.frame.get_rect()
         self.rect.center = (pos_x, pos_y)
