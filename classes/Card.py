7ac79438ba4e5ea4aa9e0de9df92cf9509e440eb
Guilherme Franca <gffranca@hotmail.com>
2022-11-09 22:59:27 -0300

    Classe Card criada

---
+++ classes/Card.py
@@ -0,0 +1,15 @@
+from pickle import FALSE
+from random import choice
+
+class Card():
+  def __init__(self, territoryId: int, isJoker: bool = False):
+    if isJoker:
+      self.turnIntoJoker()
+      return
+    typesOfCard = ['A', 'B', 'C']
+    self.type = choice(typesOfCard)
+    self.territoryId = territoryId
+    
+  def turnIntoJoker(self):
+    self.type = 'R'
+    self.territoryId = -1
