64e9c78e2240952a40cdc341e0f37b2c2d728e4f
Guilherme Franca <gffranca@hotmail.com>
2022-11-30 02:23:57 -0300

    alterao formato ia

--- classes/IA.py
+++ classes/IA.py
@@ -7,47 +7,55 @@ from classes.Dealer import *
 
 class IA():     # herda da classe player
         
-    def set_border_countries(self, gameMap: GameMap):
-        player_territories = self.set_bsrs_bsts(gameMap)
+    def get_territories(self, gameMap: GameMap, color: str):
+        player_territories = []
+        for territory in gameMap.territories:
+            if territory.color == color:
+                player_territories.append(territory)
+        return player_territories
+  
+  
+    def set_border_countries(self, gameMap: GameMap, color: str):
+        player_territories = self.set_bsrs_bsts(gameMap, color)
         result = list(filter(lambda x: x.bst != 0, player_territories))
         # print(f'BODERRR  ----  {result}')
         return result
 
-    def sort_bsr_ascendant(self, gameMap: GameMap):
-        player_territories = self.set_bsrs_bsts(gameMap)
+    def sort_bsr_ascendant(self, gameMap: GameMap, color: str):
+        player_territories = self.set_bsrs_bsts(gameMap, color)
         result = list(filter(lambda y: y.bsr < 0.6, sorted(list(filter(lambda x: x.numberOfTroops > 1, player_territories)), key=lambda country: country.bsr)))
         '''print('ORIGIN')
         for t in result:
             print(f'{t.name} e bsr {t.bsr}')'''
         return result
 
-    def sort_bsr_descendant(self, gameMap: GameMap):
-        player_territories = self.set_bsrs_bsts(gameMap)
+    def sort_bsr_descendant(self, gameMap: GameMap, color: str):
+        player_territories = self.set_bsrs_bsts(gameMap, color)
         result = list(filter(lambda y: y.bsr > 0.8, sorted(player_territories, key=lambda country: country.bsr, reverse=True)))
         '''print('TARGET')
         for t in result:
             print(f'{t.name} e bsr {t.bsr}')'''
         return result
 
-    def set_bsrs_bsts(self, gameMap: GameMap):
-        player_territories = self.get_territories(gameMap)
+    def set_bsrs_bsts(self, gameMap: GameMap, color: str):
+        player_territories = self.get_territories(gameMap, color)
         for territory in player_territories:
             territory.set_bst(gameMap.territories)
             territory.set_bsr()
         return player_territories
        
-    def initiation_attack(self, gameMap: GameMap):
-        player_territories = self.set_bsrs_bsts(gameMap)
-        borderCountries = self.set_border_countries()
+    def initiation_attack(self, gameMap: GameMap, color: str):
+        player_territories = self.set_bsrs_bsts(gameMap, color)
+        borderCountries = self.set_border_countries(gameMap, color)
         originCountries = []
         for country in borderCountries:
             # print(f'{country.name} tem {country.numberOfTroops} tropas e tem bsr igual a {country.bsr}')
             if country.bsr < 1.9:
                 originCountries.append(country)
         # return originCountries        USADO PROS TESTES UNITARIOS
-        self.main_attack(originCountries, gameMap)
+        self.main_attack(originCountries, gameMap, color)
     
-    def main_attack(self, originCountries: list[Territory], gameMap: GameMap):
+    def main_attack(self, originCountries: list[Territory], gameMap: GameMap, color: str):
         tries_left = 30 # pode ocorrer um territorio tem bsr menor do que o limite estabelecido, mas nao atacar pq tem menos tropas que o inimigo e ai entra num loop
         while originCountries and tries_left:
             country = originCountries.pop(0)
@@ -61,8 +69,8 @@ class IA():     # herda da classe player
                 # if losses[1] == targetTroops:   # isso pode sair no final, se nao tiver esse print
                     # print(f'IA {self.color}: {country.name} conquistou {target.name}\n')
             
-            player_territories = self.set_bsrs_bsts(gameMap)
-            borderCountries = self.set_border_countries()
+            player_territories = self.set_bsrs_bsts(gameMap, color)
+            borderCountries = self.set_border_countries(gameMap, color)
             originCountries = []
             for country in borderCountries:
                 # print(f'{country.name} tem {country.numberOfTroops} tropas e tem bsr igual a {country.bsr}')
@@ -71,11 +79,11 @@ class IA():     # herda da classe player
             tries_left = tries_left -1
 
 
-    def supply(self, additionalTroops: int, gameMap: GameMap):    # objetivo é deixar a distribuicao de tropas nos territorios a mais equilibrada possivel
-        player_territories = self.set_bsrs_bsts(gameMap)
-        border_countries = self.set_border_countries()
+    def supply(self, additionalTroops: int, gameMap: GameMap, color: str):    # objetivo é deixar a distribuicao de tropas nos territorios a mais equilibrada possivel
+        player_territories = self.set_bsrs_bsts(gameMap, color)
+        border_countries = self.set_border_countries(gameMap, color)
         while additionalTroops > 0:
-            player_territories = self.set_bsrs_bsts(gameMap)
+            player_territories = self.set_bsrs_bsts(gameMap, color)
             targetCountries = sorted(border_countries, key= lambda country: country.bsr, reverse=True) # ordenar pelos bsr mais altos
             # print(f'{targetCountries[0].name} tem {targetCountries[0].numberOfTroops} tropas e bsr igual a {targetCountries[0].bsr}\n')
             if targetCountries[0].gainTroops(1):
@@ -84,43 +92,43 @@ class IA():     # herda da classe player
             
         # return player_territories     USADO PROS TESTES UNITARIOS
 
-    def move(self, gameMap: GameMap):
-        player_territories = self.set_bsrs_bsts(gameMap)
+    def move(self, gameMap: GameMap, color: str):
+        player_territories = self.set_bsrs_bsts(gameMap, color)
         innerCountries =  list(filter(lambda x: x.bst == 0, player_territories))
         originCountries = list(filter(lambda country: country.numberOfTroops > 1, innerCountries))
         
         while originCountries:
             origin = originCountries.pop(0)
-            player_territories = self.set_bsrs_bsts(gameMap)
-            targetCountries = sorted(self.set_border_countries(), key= lambda country: country.bsr, reverse=True)
+            player_territories = self.set_bsrs_bsts(gameMap, color)
+            targetCountries = sorted(self.set_border_countries(gameMap, color), key= lambda country: country.bsr, reverse=True)
             for target in targetCountries:
-                path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, (origin.numberOfTroops -1))      # ideia é mandar todas as tropas para o territorio que mais precisa
+                path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, (origin.numberOfTroops -1), True)      # ideia é mandar todas as tropas para o territorio que mais precisa
                 if path != []:
                     # print(f'IA {self.color}: {origin.name} moveu suas tropas para {target.name}')
                     break
 
 
-        player_territories = self.set_bsrs_bsts(gameMap)
+        player_territories = self.set_bsrs_bsts(gameMap, color)
         # fazer a parte de movimentacao das tropas de territorios com bsr baixo para territorios com bsr alto
-        originCountries = self.sort_bsr_ascendant()
-        targetCountries = self.sort_bsr_descendant()
+        originCountries = self.sort_bsr_ascendant(gameMap, color)
+        targetCountries = self.sort_bsr_descendant(gameMap, color)
 
         while originCountries:
             origin = originCountries.pop(0)
             # print(f'origin {origin.name}')
             for target in targetCountries:
                 # print(f'target {target.name}')
-                path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, 1)
-                player_territories = self.set_bsrs_bsts(gameMap)
+                path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, 1, True)
+                player_territories = self.set_bsrs_bsts(gameMap, color)
                 if path != []:
                     # print(f'IA {self.color}: {origin.name} moveu 1 tropa para {target.name}')
                     while (origin.bsr < 0.6 and origin.numberOfTroops > 1):
-                        path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, 1)
-                        player_territories = self.set_bsrs_bsts(gameMap)
+                        path = gameMap.moveTroopsBetweenFriendlyTerrirories(origin.id, target.id, 1, True)
+                        player_territories = self.set_bsrs_bsts(gameMap, color)
                         # if path != []:
                             # print(f'IA {self.color}: {origin.name} moveu 1 tropa para {target.name}')
                     if origin.bsr > 0.6: # movimentacao 'prejudicou' a origem, deve retornar a tropa
-                            gameMap.moveTroopsBetweenFriendlyTerrirories(target.id, origin.id, 1)
+                            gameMap.moveTroopsBetweenFriendlyTerrirories(target.id, origin.id, 1, True)
                             break
 
         # return player_territories     USADO PROS TESTES UNITARIOS
